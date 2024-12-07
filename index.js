2e4a094b9687da6ca443db449998f55ec3067d9f
J-ms-R <ar2834@njit.edu>
2024-12-07 01:01:36 -0500

    Added Buttons
    Hopefully this works...

---
+++ index.js
@@ -0,0 +1,9 @@
+function Message() {
+    var text = document.getElementById("text");
+    text.style.display = "block";
+}
+
+function Message2() {
+    var text = document.getElementById("text2");
+    text.style.display = "block";
+}
